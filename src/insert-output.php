bf26db00e51b775bee25d90b45878bc426f92502
TK0606 <2201137@s.asojuku.ac.jp>
2024-01-12 12:08:32 +0900

    insert

---
+++ src/insert-output.php
@@ -0,0 +1,33 @@
+    <?php require 'header.php'; ?>
+	<?php require '../php_init/db-connect.php' ?>
+	<?php
+	
+	$sql=$db->prepare('insert into Shinkansen Values (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+	$sql->execute([$_POST["name"], $_POST["explanation"], $_POST["upload_image"], $_POST["vehicle"], 
+        $_POST["stop"], $_POST["zaseki"], $_POST["outlet"], $_POST["hanbai"], $_POST["category"]]);
+    $getfrommysql = mysql_fetch_assoc(mysql_query('SELECT * FROM Category'));
+    $data = array();
+    $i = 1;
+    foreach($getfrommysql as $value) {
+    // 一応データをパースする
+    $data[$i] = htmlspecialchars(trim(urldecode(mb_convert_encoding($value, 'UTF-8', 'auto'))));
+    $i++;
+    echo '<h1>登録しました</h1>';
+
+
+		echo '名前：', $_POST['name'],'<br>';
+		echo '説明：', $_POST["explanation"] ,'<br>';
+		echo '画像：', $_POST['upload_image'],'<br>';
+		echo '使用車両：', $_POST['vehicle'],'<br>';
+		echo '停車駅：', $_POST['stop'],'<br>';
+        echo '座席の種類：', $_POST['zaseki'],'<br>';
+        echo 'コンセントの有無：', $_POST['outlet'],'<br>';
+        echo '車内販売：', $_POST['hanbai'],'<br>';
+        echo 'カテゴリ：', $data[$i],'<br>';
+    }
+    
+    ?>
+	<br><br><br>
+    <input type="button" class="button" onclick="location.href='insert.php'" value="続けて登録する">
+    
+    <?php require 'footer.php'; ?>
--- src/insert.php
+++ src/insert.php
@@ -1,13 +1,25 @@
+<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <a href="menu.php">メニューに戻る</a>
 <hr>
+<?php
+$pdo=new PDO($connect,USER,PASS);
+$sql=$pdo->query('select * from Category');
+?>
 名前<input type="text" name="name" required><br>
 説明<textarea name="explanation" required></textarea><br>
 画像<input type="file" name="upload_image" accept="image/*" required><br>
-使用車両<input type="text" name="vehicli" required><br>
+使用車両<input type="text" name="vehicle" required><br>
 停車駅<textarea name="stop" required></textarea><br>
 座席の種類<input type="text" name="zaseki" required><br>
 コンセントの有無<textarea name="outlet" required></textarea><br>
 車内販売<input type="text" name="hanbai" required><br>
+カテゴリー<select name="category" required>
+<option value="" selected hidden required>選択してください</option>
+<?php 
+foreach($sql as $row){
+    echo '<option value="',$row['category_id'],'">',$row['category_name'],'</option></select><br>'
+}
+?>
 <input type="submit" value="登録">
 <?php require 'header.php'; ?>
